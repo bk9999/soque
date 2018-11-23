13447b64818b4410ab7e9d68bd2d85675d5be566
Bhasker Thapa <bk@live.in>
2018-11-23 13:30:45 +0530

    mysql server

---
+++ upsc/settings_secret.template.py
@@ -0,0 +1,16 @@
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY_STR = ''
+DEFAULT_DB = {
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+DEFAULT_DB_SQL_LITE = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
